e732b64c2148242ff38fe0616eebc774e854d22c
Peter Wiringa <wiringa@umn.edu>
2019-12-11 21:36:38 -0600

    try CARTO Positron basemap

--- client/src/components/OpenLayers/BasemapLayer.js
+++ client/src/components/OpenLayers/BasemapLayer.js
@@ -8,7 +8,7 @@ export default new TileLayer({
   name: "tiledBasemap",
   source: new XYZ({
     url:
-      "https://cartodb-basemaps-{a-d}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+      "https://{1-4}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png",
     attributions:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attribution/">CartoDB</a>'
   })
